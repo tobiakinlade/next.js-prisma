9a9b4f9fe5979b1632d5f8b648aac4eee610f7eb
tobiakinlade <tobi.akinlade@yahoo.com>
2022-05-11 15:59:34 +0100

    final

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,38 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import prisma from 'lib/prisma';
 
-export default function Home({ cars }) {
+export default function Home({ cars, firstCar }) {
   return (
     <div className={styles.container}>
-      {cars.map((car) => {
-        return (
-          <li key={car.id}>
-            {car.brand} - {car.model}
-          </li>
-        );
-      })}
+      <p>
+        First car: {firstCar.brand} - {firstCar.model}
+      </p>
+      <ul>
+        {cars.map((car) => {
+          return (
+            <li key={car.id}>
+              {car.brand} - {car.model}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
 export async function getServerSideProps() {
   let cars = await prisma.car.findMany();
-  cars = JSON.parse(JSON.stringify(cars));
+  let firstCar = await prisma.car.findUnique({
+    where: {
+      id: 1,
+    },
+  });
+  cars = JSON.parse(JSON.stringify(cars)); // convert dates to string
+  firstCar = JSON.parse(JSON.stringify(firstCar)); // convert dates to string
   return {
     props: {
       cars,
+      firstCar,
     },
   };
 }
